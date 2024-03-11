18e30e47742cd0340382e3fc2706485acd7f29f6
Alex Jamison <business.homeit@gmail.com>
2024-03-11 15:53:00 +0300

    remove auth

--- src/app/api/car/route.ts
+++ src/app/api/car/route.ts
@@ -1,13 +1,8 @@
 import { Car } from '@/types';
 import create from '@/utils/crud/create';
-import { auth } from '@clerk/nextjs';
 
 export async function POST(req: Request) {
 	try {
-		const { userId } = auth();
-		if (!userId) {
-			return new Response('Unauthorized', { status: 401 });
-		}
 		const car = await req.json();
 		await create(car as Omit<Car, 'id'>);
 		return Response.json(true);
