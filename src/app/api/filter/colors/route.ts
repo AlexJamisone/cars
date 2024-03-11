7a2fb5922b64d75982eaaf2968dfbcd6302b9ca8
Alex Jamison <business.homeit@gmail.com>
2024-03-11 17:22:22 +0300

    try solve problems with filters api

--- src/app/api/filter/colors/route.ts
+++ src/app/api/filter/colors/route.ts
@@ -1,12 +1,14 @@
 import { getColors } from '@/helpers/getUniq';
 import read from '@/utils/crud/read';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
 	try {
 		const cars = await read({ sleep: false });
 		const { colors } = getColors(cars ?? []);
-		return Response.json(colors);
+		return NextResponse.json(colors);
 	} catch (err) {
 		console.log(err);
 	}
 }
+export async function POST(req: NextRequest) {}
--- src/app/api/filter/route.ts
+++ src/app/api/filter/route.ts
@@ -1,12 +1,14 @@
 import { getBrands } from '@/helpers/getUniq';
 import read from '@/utils/crud/read';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
 	try {
 		const cars = await read();
 		const { brands } = getBrands(cars ?? []);
-		return Response.json(brands);
+		return NextResponse.json(brands);
 	} catch (err) {
 		console.log(err);
 	}
 }
+export async function POST(req: NextRequest) {}
