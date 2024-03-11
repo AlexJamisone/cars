355c9a3966253828f81672a89dbd6c5ff2fa6439
Alex Jamison <business.homeit@gmail.com>
2024-03-11 17:10:20 +0300

    add async to colors and brands

--- src/app/api/filter/route.ts
+++ src/app/api/filter/route.ts
@@ -3,7 +3,7 @@ import read from '@/utils/crud/read';
 
 export async function GET() {
 	try {
-		const cars = await read({ sleep: false });
+		const cars = await read();
 		const { brands } = getBrands(cars ?? []);
 		return Response.json(brands);
 	} catch (err) {
